f8c65ce5069f2e370c0a37cc1c8aeb0eb931f909
masassuncao <marcoassuncao.eng@gmail.com>
2023-10-26 19:28:01 -0300

    Extracao dos nomes das filas para o arquivo .env

--- src/services/servicoCarteiras.js
+++ src/services/servicoCarteiras.js
@@ -1,12 +1,17 @@
 const mq = require('./servicoMQ')
 
+// Importa o módulo dotenv
+const env = require('dotenv').config()
+const {NOME_FILA_CRIAR_CARTEIRA: FILA_CRIAR_CARTEIRA} = process.env
+const {NOME_FILA_EXCLUIR_CARTEIRA: FILA_EXCLUIR_CARTEIRA} = process.env
+
 function criarCarteira (idUsuario) {
-    mq.sendToQueue("filaCriarCarteiras", JSON.stringify(idUsuario));
+    mq.sendToQueue(FILA_CRIAR_CARTEIRA, JSON.stringify(idUsuario));
     console.log("Criando carteira para usuário " + idUsuario)
 }
 
 function excluirCarteira (idUsuario) {
-    mq.sendToQueue("filaExcluirCarteiras", JSON.stringify(idUsuario));
+    mq.sendToQueue(FILA_EXCLUIR_CARTEIRA, JSON.stringify(idUsuario));
     console.log("Excluindo carteira do usuário " + idUsuario)
 }
 
